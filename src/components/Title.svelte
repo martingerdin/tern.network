5003df9a166513487ca2e07a4da6bb8cca92c2d3
Martin Gerdin Warnberg <martingerdin@gmail.com>
2022-04-19 16:59:36 +0200

    Style title text

--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -1,10 +1,24 @@
 <style>
  @import "../styles/bulma.min.css";
+
+ div {
+     margin: -4rem 4rem 4rem 4rem;
+ }
+ 
+ h1 {
+     font-size: 4rem;
+     z-index: 2;
+     transform: skew(-7deg);
+ }
+
+ span {
+     background: black;
+     padding: 0.5rem;
+ }
 </style>
 
-<h1 class="title is-size-1">
-    Trauma life support<br />
-    Effectiveness<br />
-    Research<br />
-    Network
-</h1>
+<div class="has-text-centered">
+    <h1 class="title">
+	<span>&nbsp;Researching Trauma Life Support Training Effectiveness&nbsp;</span>
+    </h1>
+</div>
