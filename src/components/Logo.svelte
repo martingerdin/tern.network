0a2c23fdd3ce517b24c4bd9d91fbbd5086b614cb
Martin Gerdin Warnberg <martingerdin@gmail.com>
2022-04-19 10:08:06 +0200

    Create logo

---
+++ src/components/Logo.svelte
@@ -0,0 +1,10 @@
+<style>
+ @import url('https://fonts.googleapis.com/css2?family=Oxygen:wght@700&display=swap');
+
+ p {
+     font-size: 3rem;
+     font-family: 'Oxygen', sans-serif;
+ }
+</style>
+
+<p>TERN</p>
