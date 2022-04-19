23eb131d646f3edd74b9106eaee9add7c2ddf971
Martin Gerdin Warnberg <martingerdin@gmail.com>
2022-04-19 10:20:52 +0200

    Add navbar

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,22 @@
+<script>
+ import Logo from "./Logo.svelte";
+</script>
+
+<nav class="level">
+
+    <p class="level-item has-text-centered">
+	<!-- <a class="link is-info">Home</a> -->
+    </p>
+    <p class="level-item has-text-centered">
+	<a class="link is-info">About</a>
+    </p>
+    <p class="level-item has-text-centered">
+	<Logo />
+    </p>
+    <p class="level-item has-text-centered">
+	<a class="link is-info">News</a>
+    </p>
+    <p class="level-item has-text-centered">
+	<!-- <a class="link is-info">Contact</a> -->
+    </p>
+</nav>
