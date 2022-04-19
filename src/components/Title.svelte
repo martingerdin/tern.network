9c4f4fdea59abdd697d30e3bedd707af7f27f245
Martin Gerdin Warnberg <martingerdin@gmail.com>
2022-04-19 10:46:06 +0200

    Add title component

---
+++ src/components/Title.svelte
@@ -0,0 +1,14 @@
+<style>
+ @import "../styles/bulma.min.css";
+
+ h1 {
+     min-width: 500px;
+ }
+</style>
+
+<h1 class="title is-size-1">
+    Trauma life support<br />
+    Effectiveness<br />
+    Research<br />
+    Network
+</h1>
