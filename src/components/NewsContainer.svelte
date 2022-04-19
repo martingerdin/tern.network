c7de2a571d7fceac537194b8826510e087c56707
Martin Gerdin Warnberg <martingerdin@gmail.com>
2022-04-19 14:44:02 +0200

    Move news to own file

--- src/components/NewsContainer.svelte
+++ src/components/NewsContainer.svelte
@@ -1,5 +1,6 @@
 <script>
  import NewsItem from "../components/NewsItem.svelte";
+ import { news } from "../data/news.js";
 </script>
 
 <style>
@@ -9,24 +10,22 @@
 <div>
     <h3 class="title">Latest News</h3>
     <div class="box">
-	<NewsItem src="pilot-protocol-bmjopen.png">
-	    <span slot="title">
-		Trial pilot protocol published in BMJ Open!
-	    </span>
-	    <span slot="author">
-		@tern
-	    </span>
-	    <span slot="date">
-		2022-04-18
-	    </span>
-	    <span slot="content">
-		The TERN pilot protocol titled "A pilot multicentre
-		cluster randomised trial to compare the effect of
-		trauma life support training programmes on patient and
-		provider outcomes" was just published in BMJ
-		Open. <a href="https://bmjopen.bmj.com/content/12/4/e057504.full">Read the full publication here</a>.</span>
-	</NewsItem>
-	<NewsItem />
+	{#each news as item}
+	    <NewsItem src={item.src}>
+		<span slot="title">
+		    {item.title}
+		</span>
+		<span slot="author">
+		    @{item.author}
+		</span>
+		<span slot="date">
+		    {item.date}
+		</span>
+		<span slot="content">
+		    {item.content}
+		</span>
+	    </NewsItem>
+	{/each}
     </div>
     <div class="buttons">
 	<button class="button is-outlined is-info is-inverted">
---
+++ src/data/news.js
@@ -0,0 +1,18 @@
+export const news = [
+  {
+    src: "pilot-protocol-bmjopen.png",
+    title: "Trial pilot protocol published in BMJ Open!",
+    author: "tern",
+    date: "2022-04-18",
+    content: "The TERN pilot protocol titled \"A pilot multicentre cluster randomised trial to compare the effect of trauma life support training programmes on patient and provider outcomes\" was just published in BMJ Open.",
+    url: "https://bmjopen.bmj.com/content/12/4/e057504.full",
+  },
+  {
+    src: "",
+    title: "Test",
+    author: "tester",
+    date: "2022-04-18",
+    content: "Test content",
+    url: "",
+  },
+];
